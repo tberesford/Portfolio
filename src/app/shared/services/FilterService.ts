0ebd8bb6fdf7b0bc33c47334732764bef67a2acf
Tom Beresford <tberesfords@outook.com>
2025-01-08 11:47:20 +0000

    Solar Tests

--- src/app/shared/services/FilterService.ts
+++ src/app/shared/services/FilterService.ts
@@ -1,6 +1,9 @@
-export async function FilterData<T>(data: T[], property: keyof T, index: keyof T){
+export async function FilterData<T>(data: T[], properties: (keyof T)[], index: keyof T){
     return data.map(obj => ({
         [index]: obj[index],
-        [property]: obj[property]
+        ...properties.reduce((result, property) => {
+            result[property] = obj[property];
+            return result;
+        }, {} as Partial<T>)
     }));
 }
--- src/app/tests/api_tests/solar.test.tsx
+++ src/app/tests/api_tests/solar.test.tsx
@@ -1,22 +1,31 @@
+import { FilterData } from "@/app/shared/services/FilterService";
 import { SolarModel } from "@/app/solar/interfaces/solarmodel";
 import axios from "axios";
-import exp from "constants";
-
 
 jest.mock('axios');
 const mockedAxios = axios as jest.Mocked<typeof axios>;
 
-const mockedData: SolarModel[] = [{
-    TS: new Date(),
-    GRID: 0,
-    SOLAR: 0,
-    PERCENT_CHARGED: 0,
-    LOAD: 0,
-    RESERVE: 0
-}];
-
 describe('/api/solar', () => {
-    it("Should return a 200 response for /api/solar?dayrange=1", async () => {
+    let mockedData: SolarModel[]; 
+    let mockedFilteredData: Partial<SolarModel>[];
+    beforeAll(() => {
+        mockedData = [{
+            TS: new Date(),
+            GRID: 0,
+            SOLAR: 0,
+            PERCENT_CHARGED: 0,
+            LOAD: 0,
+            RESERVE: 0
+        }];
+
+        mockedFilteredData = [{
+            TS: new Date(),
+            GRID: 0,
+            SOLAR: 0
+        }];
+    })
+
+    it("Should return a 200 response", async () => {
         mockedAxios.get.mockResolvedValueOnce({data: mockedData, status: 200});
         const response = await axios.get("/api/solar?dayrange=1");
         expect(response.status).toEqual(200);
@@ -28,7 +37,12 @@ describe('/api/solar', () => {
         expect(response.data).toEqual(mockedData);
     });
 
-    test.todo("Should filter data");
+    it("Should filter data", async () => {
+        mockedAxios.get.mockResolvedValueOnce({data: mockedData, status: 200});
+        const response = await axios.get("/api/solar?dayrange=1");
+        const filteredData = await FilterData(response.data, ['SOLAR', 'GRID'], 'TS');
+        expect(filteredData).toEqual(mockedFilteredData);
+    });
 
     test.todo("Should return different datasets");
 })
